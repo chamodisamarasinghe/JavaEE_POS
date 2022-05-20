e49c7fb08c8ed0d31ed5820aec1f60c4adf53a29
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 11:51:09 +0530

    Updated filr

--- BackEnd/src/dao/custom/CustomerDAO.java
+++ BackEnd/src/dao/custom/CustomerDAO.java
@@ -1,6 +1,11 @@
 package dao.custom;
 
 import dao.CrudDAO;
+import entity.Customer;
 
-public interface CustomerDAO extends CrudDAO {
+import java.sql.SQLException;
+
+public interface CustomerDAO extends CrudDAO<Customer, String> {
+    boolean ifCustomerExist(String id) throws SQLException, ClassNotFoundException;
+    String generateNewID() throws SQLException, ClassNotFoundException;
 }
