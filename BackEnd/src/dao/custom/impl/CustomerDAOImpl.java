ea104c16dc6f8b5a8b268bc47c41c27cf448e593
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 13:27:18 +0530

    Updated file

--- BackEnd/src/dao/custom/impl/CustomerDAOImpl.java
+++ BackEnd/src/dao/custom/impl/CustomerDAOImpl.java
@@ -1,6 +1,58 @@
 package dao.custom.impl;
 
+import dao.CrudUtil;
 import dao.custom.CustomerDAO;
+import entity.Customer;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public boolean ifCustomerExist(String id) throws SQLException, ClassNotFoundException {
+        return CrudUtil.executeQuery("SELECT customerId FROM Customer WHERE id=?", id).next();
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean add(Customer customer) throws SQLException, ClassNotFoundException {
+        return CrudUtil.executeUpdate("INSERT INTO Customer (id,name,address,salary) VALUES (?,?,?,?)", customer.getId(), customer.getName(), customer.getAddress(), customer.getSalary()
+        );
+
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return CrudUtil.executeUpdate("DELETE FROM Customer WHERE customerId=?", id);
+    }
+
+    @Override
+    public boolean update(Customer customer) throws SQLException, ClassNotFoundException {
+        return CrudUtil.executeUpdate("UPDATE Customer SET name=?,  address=?, salary=?", customer.getName(),  customer.getAddress(), customer.getSalary(), customer.getId());
+
+    }
+
+    @Override
+    public Customer search(String id) throws SQLException, ClassNotFoundException {
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM Customer WHERE id=?", id);
+        rst.next();
+        return new Customer(id,  rst.getString("name"), rst.getString("address"), rst.getDouble("salary"));
+
+    }
+
+    @Override
+    public ArrayList<Customer> getAll() throws SQLException, ClassNotFoundException {
+        ArrayList<Customer> allCustomers = new ArrayList();
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM Customer");
+        while (rst.next()) {
+            allCustomers.add(new Customer(rst.getString("id"),  rst.getString("name"), rst.getString("address"), rst.getDouble("salary")));
+
+        }
+        return allCustomers;
+    }
 }
