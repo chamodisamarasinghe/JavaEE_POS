a504652487c94d76be4ef4d672dd9dddd3e72409
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 19:23:43 +0530

    Updated

--- BackEnd/src/bo/custom/CustomerBO.java
+++ BackEnd/src/bo/custom/CustomerBO.java
@@ -1,4 +1,22 @@
 package bo.custom;
 
-public interface CustomerBO {
+import bo.SuperBO;
+import dto.CustomerDTO;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface CustomerBO extends SuperBO {
+    boolean addCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException;
+
+    ObservableList<CustomerDTO> getAllCustomer(Connection connection) throws SQLException, ClassNotFoundException;
+
+    CustomerDTO searchCustomer(String id, Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean deleteCustomer(Connection connection, String id) throws SQLException, ClassNotFoundException;
+
+    boolean updateCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException;
+
+
 }
