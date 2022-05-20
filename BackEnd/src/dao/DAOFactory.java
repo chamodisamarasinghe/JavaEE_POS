737f4d65dc42bff4a3301ec509895bfa36dcde86
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 19:15:41 +0530

    Updated dao factory

--- BackEnd/src/dao/DAOFactory.java
+++ BackEnd/src/dao/DAOFactory.java
@@ -6,35 +6,31 @@ public class DAOFactory {
     private static DAOFactory daoFactory;
 
     private DAOFactory() {
+
     }
 
-    public static DAOFactory getDAOFactory() {
+    public static DAOFactory getDaoFactory() {
         if (daoFactory == null) {
             daoFactory = new DAOFactory();
         }
         return daoFactory;
     }
 
+    public SuperDAO getDAO(DAOTypes types){
 
-    public SuperDAO getDAO(DAOTypes types) {
-        switch (types) {
-            case CUSTOMER:
-                return new CustomerDAOImpl();
+        switch (types){
 //            case ITEM:
 //                return new ItemDAOImpl();
-//            case ORDER:
-//                return new OrderDAOImpl();
-//            case ORDERDETAIL:
-//                return new OrderDetailsDAOImpl();
-//            case QUERYDAO:
-//                return new QueryDAOImpl();
-                default:
+            case CUSTOMER:
+                return new CustomerDAOImpl();
+            default:
                 return null;
         }
+
     }
 
-    public enum DAOTypes {
-        CUSTOMER, ITEM, ORDER, ORDERDETAIL, QUERYDAO
+    public enum DAOTypes{
+        CUSTOMER, ITEM, ORDERS, ORDERDETAILS
     }
 
 }
