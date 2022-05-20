162920d210574a899ebcfc5f27f9ac2fc10a709e
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 10:25:08 +0530

    Added item entity

--- BackEnd/src/entity/Item.java
+++ BackEnd/src/entity/Item.java
@@ -1,4 +1,50 @@
 package entity;
 
 public class Item {
+    String code;
+    String item;
+    int qty;
+    double price;
+
+    public Item() {
+    }
+
+    public Item(String code, String item, int qty, double price) {
+        this.code = code;
+        this.item = item;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
