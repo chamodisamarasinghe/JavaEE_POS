38be30a4b48d00690b2ae92f4594af9354336930
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 19:08:12 +0530

    Added dto

--- BackEnd/src/dto/CustomerDTO.java
+++ BackEnd/src/dto/CustomerDTO.java
@@ -1,4 +1,60 @@
 package dto;
 
 public class CustomerDTO {
+    private String cusId;
+    private String cusName;
+    private String cusAddress;
+    private String cusSalary;
+
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(String cusId, String cusName, String cusAddress, String cusSalary) {
+        this.cusId = cusId;
+        this.cusName = cusName;
+        this.cusAddress = cusAddress;
+        this.cusSalary = cusSalary;
+    }
+
+    public String getCusId() {
+        return cusId;
+    }
+
+    public void setCusId(String cusId) {
+        this.cusId = cusId;
+    }
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    public String getCusAddress() {
+        return cusAddress;
+    }
+
+    public void setCusAddress(String cusAddress) {
+        this.cusAddress = cusAddress;
+    }
+
+    public String getCusSalary() {
+        return cusSalary;
+    }
+
+    public void setCusContact(String cusContact) {
+        this.cusSalary = cusSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                "cusId='" + cusId + '\'' +
+                ", cusName='" + cusName + '\'' +
+                ", cusAddress='" + cusAddress + '\'' +
+                ", cusContact='" + cusSalary + '\'' +
+                '}';
+    }
 }
