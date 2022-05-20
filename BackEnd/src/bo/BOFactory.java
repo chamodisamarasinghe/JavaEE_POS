93500ab7a616521228033c257752c1b00c3a2455
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 19:17:08 +0530

    Added BOFactory

--- BackEnd/src/bo/BOFactory.java
+++ BackEnd/src/bo/BOFactory.java
@@ -1,4 +1,34 @@
 package bo;
 
+import bo.custom.impl.CustomerBOImpl;
+import bo.custom.impl.ItemBOImpl;
+
 public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory(){
+
+    }
+
+    public static BOFactory getBoFactory(){
+        if (boFactory == null){
+            boFactory = new BOFactory();
+        }
+        return  boFactory;
+    }
+
+    public SuperBO getBO(BOTypes types){
+        switch (types){
+            case CUSTOMER:
+                return new CustomerBOImpl();
+//            case ITEM:
+//                return new ItemBOImpl();
+            default:
+                return null;
+        }
+    }
+
+    public enum BOTypes{
+        CUSTOMER, ITEM, ORDERS, ORDERDETAILS
+    }
 }
