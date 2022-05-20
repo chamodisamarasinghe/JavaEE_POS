d5b86a7b009181e76a3a82d5d8fac01ccc1d92c4
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 20:05:45 +0530

    Updated CustomerDAOImpl

--- BackEnd/src/dao/custom/impl/CustomerDAOImpl.java
+++ BackEnd/src/dao/custom/impl/CustomerDAOImpl.java
@@ -3,56 +3,66 @@ package dao.custom.impl;
 import dao.CrudUtil;
 import dao.custom.CustomerDAO;
 import entity.Customer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CustomerDAOImpl implements CustomerDAO {
-    @Override
-    public boolean ifCustomerExist(String id) throws SQLException, ClassNotFoundException {
-        return CrudUtil.executeQuery("SELECT customerId FROM Customer WHERE id=?", id).next();
-    }
 
     @Override
-    public String generateNewID() throws SQLException, ClassNotFoundException {
-        return null;
-    }
-
-    @Override
-    public boolean add(Customer customer) throws SQLException, ClassNotFoundException {
-        return CrudUtil.executeUpdate("INSERT INTO Customer (id,name,address,salary) VALUES (?,?,?,?)", customer.getId(), customer.getName(), customer.getAddress(), customer.getSalary()
-        );
+    public boolean add(Customer customer, Connection connection) throws SQLException, ClassNotFoundException {
+        return CrudUtil.executeUpdate(connection,"INSERT INTO Customer VALUES(?,?,?,?)",customer.getId(),
+                customer.getName(),customer.getAddress(),customer.getSalary());
 
     }
 
     @Override
-    public boolean delete(String id) throws SQLException, ClassNotFoundException {
-        return CrudUtil.executeUpdate("DELETE FROM Customer WHERE customerId=?", id);
+    public boolean delete(String id, Connection connection) throws SQLException, ClassNotFoundException {
+        return CrudUtil.executeUpdate(connection,"DELETE FROM Customer WHERE id=?", id);
     }
 
     @Override
-    public boolean update(Customer customer) throws SQLException, ClassNotFoundException {
-        return CrudUtil.executeUpdate("UPDATE Customer SET name=?,  address=?, salary=?", customer.getName(),  customer.getAddress(), customer.getSalary(), customer.getId());
-
+    public boolean update(Customer customer, Connection connection) throws SQLException, ClassNotFoundException {
+        return CrudUtil.executeUpdate(connection, "UPDATE Customer SET name=?,address=?,contact=? WHERE id=?",customer.getName(),
+                customer.getAddress(),customer.getSalary(),customer.getId());
     }
 
     @Override
-    public Customer search(String id) throws SQLException, ClassNotFoundException {
-        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM Customer WHERE id=?", id);
-        rst.next();
-        return new Customer(id,  rst.getString("name"), rst.getString("address"), rst.getDouble("salary"));
-
-    }
-
-    @Override
-    public ArrayList<Customer> getAll() throws SQLException, ClassNotFoundException {
-        ArrayList<Customer> allCustomers = new ArrayList();
-        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM Customer");
-        while (rst.next()) {
-            allCustomers.add(new Customer(rst.getString("id"),  rst.getString("name"), rst.getString("address"), rst.getDouble("salary")));
-
+    public Customer search(String id, Connection connection) throws SQLException, ClassNotFoundException {
+        ResultSet rst = CrudUtil.executeQuery(connection,"SELECT * FROM Customer WHERE id =?",id);
+        if (rst.next()){
+            return new Customer(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4)
+            );
+        }else {
+            return null;
         }
-        return allCustomers;
+    }
+
+    @Override
+    public ObservableList<Customer> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery(connection, "SELECT * FROM Customer");
+
+        ObservableList<Customer> obList = FXCollections.observableArrayList();
+
+        while (resultSet.next()) {
+            Customer customer = new Customer(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4)
+            );
+
+            obList.add(customer);
+        }
+
+        return obList;
     }
 }
