0edf37b5258bf041845899d97a24bd2d743312c1
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-05-20 19:50:26 +0530

    Updated files

--- BackEnd/src/dao/CrudDAO.java
+++ BackEnd/src/dao/CrudDAO.java
@@ -1,16 +1,21 @@
 package dao;
 
+import entity.Customer;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface CrudDAO<T, ID> extends SuperDAO{
-    boolean add(T t) throws SQLException, ClassNotFoundException;
+    boolean add(T t, Connection connection) throws SQLException, ClassNotFoundException;
 
-    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+    boolean delete(ID id, Connection connection) throws SQLException, ClassNotFoundException;
 
-    boolean update(T t) throws SQLException, ClassNotFoundException;
+    boolean update(T t, Connection connection) throws SQLException, ClassNotFoundException;
 
-    T search(ID id) throws SQLException, ClassNotFoundException;
+    T search(ID id, Connection connection) throws SQLException, ClassNotFoundException;
 
-    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+    ObservableList<Customer> getAll(Connection connection) throws SQLException, ClassNotFoundException;
+
 }
--- BackEnd/src/dao/custom/CustomerDAO.java
+++ BackEnd/src/dao/custom/CustomerDAO.java
@@ -6,6 +6,5 @@ import entity.Customer;
 import java.sql.SQLException;
 
 public interface CustomerDAO extends CrudDAO<Customer, String> {
-    boolean ifCustomerExist(String id) throws SQLException, ClassNotFoundException;
-    String generateNewID() throws SQLException, ClassNotFoundException;
+
 }
